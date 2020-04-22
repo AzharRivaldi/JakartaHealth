1cf606ccf8a89378316e3eb542a521a1d5ea41ee
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2020-04-22 21:10:51 +0700

    add token

--- JakartaHealth/app/src/main/java/com/azhar/jakartahealth/activities/HospitalKhususActivity.java
+++ JakartaHealth/app/src/main/java/com/azhar/jakartahealth/activities/HospitalKhususActivity.java
@@ -74,7 +74,7 @@ public class HospitalKhususActivity extends AppCompatActivity implements Hospita
     private void loadJSON() {
         progressDialog.show();
         AndroidNetworking.get("http://api.jakarta.go.id/v1/rumahsakitkhusus")
-                .addHeaders("Authorization", "YOUR TOKEN")
+                .addHeaders("Authorization", "5h30dB4K4Uwuhj4KkmHmFuOgeIeo+XxK4jKRm/v5lNNfbGfsYx2wB2D4IKQWaSu7")
                 .setPriority(Priority.HIGH)
                 .build()
                 .getAsJSONObject(new JSONObjectRequestListener() {
